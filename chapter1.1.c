29a0aad823fc80e0d6ab3785cb890e03147cadbe
noi <139059099+akihikosglove@users.noreply.github.com>
2023-07-16 10:01:54 -0400

    Create chapter1.1.c

---
+++ chapter1.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // header file that allows for input and output
+
+int main(){ // function that allows program to execute
+  printf("hello, world"); //prints hello world to terminal
+  return 0; //returns 0 if program works
+}
+
+// Question 1: Experiment with leaving parts of your program out. Print out the error messages.
+
+
+// Question 2: Experiment to find out what happens when using \c as an escape sequence.
